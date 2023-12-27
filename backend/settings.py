dd06543e0d13ae3dfe680c6feac2cf59f76707d9
wassimwazzi <wassim.wazzi@mail.mcgill.ca>
2023-12-26 22:31:26 -0500

    add total pages pagination

--- backend/settings.py
+++ backend/settings.py
@@ -96,7 +96,7 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
         # other permission classes if needed
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_PAGINATION_CLASS": "paginator.PageNumberPaginationWithCount",
     "PAGE_SIZE": 10,
 }
 
