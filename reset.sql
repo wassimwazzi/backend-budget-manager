be80e47a8a0462b95baa4bf32e8b720e8614fea3
wassimwazzi <76567798+wassimwazzi@users.noreply.github.com>
2024-02-25 18:42:23 -0500

    Update queryset_mixin.py

---
+++ reset.sql
@@ -0,0 +1,6 @@
+DELETE FROM category_category;
+DELETE FROM currency_currency;
+DELETE FROM budget_budget;
+DELETE FROM fileupload_fileupload;
+DELETE FROM transaction_transaction;
+DELETE FROM auth_user;
