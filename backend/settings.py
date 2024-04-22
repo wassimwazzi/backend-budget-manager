007cac700c726a6debd79da432175c3e2b9f282c
wassimwazzi <wassim.wazzi@mail.mcgill.ca>
2024-04-22 14:42:41 -0400

    test fix staticfiles

--- backend/settings.py
+++ backend/settings.py
@@ -190,8 +190,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = "static/"
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
