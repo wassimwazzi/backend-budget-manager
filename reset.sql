32e9ffdb897dadb7c1db1215b1b529d6ef076dd6
wassimwazzi <wassim.wazzi@mail.mcgill.ca>
2024-04-29 06:50:52 -0400

    cleanup

--- reset.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM category_category;
-DELETE FROM currency_currency;
-DELETE FROM budget_budget;
-DELETE FROM fileupload_fileupload;
-DELETE FROM transaction_transaction;
-DELETE FROM auth_user;
