0b48c11bb944896553f7fbb6cc2227e45c0f84de
wassimwazzi <wassim.wazzi@mail.mcgill.ca>
2024-04-29 06:46:39 -0400

    set to in progress if today

--- goal/tests/test_commands.py
+++ goal/tests/test_commands.py
@@ -34,7 +34,7 @@ class TestUpdateStatus(TestCase):
         self.assertEqual(goal.status, GoalStatus.COMPLETED)
 
     def test_sets_status_to_in_progress(self):
-        goal = GoalFactory(status=GoalStatus.PENDING, start_date=datetime.date.today() - datetime.timedelta(days=1))
+        goal = GoalFactory(status=GoalStatus.PENDING, start_date=datetime.date.today())
         call_command("update_status", stdout=StringIO())
         goal.refresh_from_db()
         self.assertEqual(goal.status, GoalStatus.IN_PROGRESS)
