8e25eb02f8b986e5eb3c6280bdded9d4257d5317
wassimwazzi <wassim.wazzi@mail.mcgill.ca>
2024-04-29 07:17:34 -0400

    load dotenv

--- backend/settings.py
+++ backend/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 import os
 import dj_database_url
 import secrets
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
