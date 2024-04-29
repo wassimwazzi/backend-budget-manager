6bc47e01c3b5f6ac75c3ad3d60351c4ad19e90b7
wassimwazzi <wassim.wazzi@mail.mcgill.ca>
2024-04-28 21:43:58 -0400

    plaid link working:

--- plaidapp/views.py
+++ plaidapp/views.py
@@ -1,54 +1,76 @@
-import json
-import time
-from datetime import date, timedelta
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-import plaid
-from plaid.model.products import Products
-from plaid.model.country_code import CountryCode
+from rest_framework import viewsets
+from rest_framework import serializers
+from django.conf import settings
+from rest_framework.response import Response
+from .serializers import PlaidItemSerializer
+from .models import PlaidItem
+from queryset_mixin import QuerysetMixin
+from rest_framework.decorators import action
+
+from plaid.model.link_token_create_request import LinkTokenCreateRequest
+from plaid.model.link_token_create_request_user import LinkTokenCreateRequestUser
 from plaid.model.item_public_token_exchange_request import (
     ItemPublicTokenExchangeRequest,
 )
-from plaid.model.link_token_create_request import LinkTokenCreateRequest
-from plaid.model.link_token_create_request_user import LinkTokenCreateRequestUser
-from plaid.model.auth_get_request import AuthGetRequest
-from plaid.model.transactions_sync_request import TransactionsSyncRequest
-from plaid.model.identity_get_request import IdentityGetRequest
-from plaid.model.link_token_create_request_statements import (
-    LinkTokenCreateRequestStatements,
-)
-
-# Fill in your Plaid API keys - https://dashboard.plaid.com/account/keys
-PLAID_CLIENT_ID = "654fe157dd1446001b7ea45f"
-PLAID_SECRET = "ad93fbb7a6b9071c695fcd1268f25d"
-# Use 'sandbox' to test with Plaid's Sandbox environment (username: user_good,
-# password: pass_good)
-# Use `development` to test with live users and credentials and `production`
-# to go live
-PLAID_ENV = "development"
-# PLAID_PRODUCTS is a comma-separated list of products to use when initializing
-# Link. Note that this list must contain 'assets' in order for the app to be
-# able to create and retrieve asset reports.
-PLAID_PRODUCTS = ["transactions"]
-
-# PLAID_COUNTRY_CODES is a comma-separated list of countries for which users
-# will be able to select institutions from.
-PLAID_COUNTRY_CODES = ["US"]
-
-configuration = plaid.Configuration(
-    host=plaid.Environment.Development,
-    api_key={
-        "clientId": PLAID_CLIENT_ID,
-        "secret": PLAID_SECRET,
-        "plaidVersion": "2020-09-14",
-    },
-)
-
-client = plaid.PlaidApi(plaid.ApiClient(configuration))
-
-products = [Products(product) for product in PLAID_PRODUCTS]
+from plaid.model.products import Products
+from plaid.model.country_code import CountryCode
+from .utils import client
 
 
-class PlaidView(
-    
-):
+class PlaidItemView(QuerysetMixin, viewsets.ModelViewSet):
+    serializer_class = PlaidItemSerializer
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the plaid items
+        for the currently authenticated user.
+        """
+        user = self.request.user
+        queryset = PlaidItem.objects.filter(user=user)
+        return self.get_filtered_queryet(queryset)
+
+    @action(detail=False, methods=["post"])
+    def create_link_token(self, request):
+        """
+        Create a link token for the user
+        """
+        print(settings.PLAID_ENV)
+        # Get the client_user_id by searching for the current user
+        user = request.user
+        client_user_id = str(user.id)
+        # Create a link_token for the given user
+        request = LinkTokenCreateRequest(
+            products=[Products("auth")],
+            client_name="Budget Manager",
+            country_codes=[CountryCode("CA"), CountryCode("US")],
+            # redirect_uri=settings.PLAID_REDIRECT_URI,
+            language="en",
+            # webhook='https://webhook.example.com',
+            user=LinkTokenCreateRequestUser(client_user_id=client_user_id),
+        )
+        response = client.link_token_create(request)
+        return Response(response.to_dict())
+
+    @action(detail=False, methods=["post"])
+    def exchange_public_token(self, request):
+        """
+        Exchange a public token for an access token
+        """
+        public_token = request.data.get("public_token")
+        print("wawawaw")
+        print(public_token)
+        user = request.user
+        request = ItemPublicTokenExchangeRequest(public_token=public_token)
+        response = client.item_public_token_exchange(request)
+        access_token = response["access_token"]
+        item_id = response["item_id"]
+        # Check if the item already exists
+        if PlaidItem.objects.filter(item_id=item_id, user=user).exists():
+            item = PlaidItem.objects.get(item_id=item_id, user=user)
+            return Response(PlaidItemSerializer(item).data)
+        item = PlaidItem.objects.create(
+            user=user,
+            access_token=access_token,
+            item_id=item_id,
+        )
+        return Response(PlaidItemSerializer(item).data)
